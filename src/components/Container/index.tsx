e0fe7be7a583626059278b65243e2d1f985f7275
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-09-20 17:24:58 -0300

    feat: Adiciona a prop 'noPadding' ao componente Container para controle de espaçamento

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -4,18 +4,24 @@ import { twMerge } from "tailwind-merge";
 type ContainerProps<E extends React.ElementType = "div"> = {
   as?: E;
   className?: string;
+  noPadding?: boolean;
 } & React.ComponentPropsWithoutRef<E>;
 
 export function Container<E extends React.ElementType = "div">({
   as,
   className,
   children,
+  noPadding,
   ...rest
 }: ContainerProps<E>) {
   const Component = as || "div";
   return (
     <Component
-      className={twMerge("p-4 bg-white rounded shadow-sm", className)}
+      className={twMerge(
+        "p-4 bg-white rounded shadow-sm",
+        noPadding && "p-0",
+        className
+      )}
       {...rest}
     >
       {children}
