f491614dd52924121d6822123a5bc43bf9c35861
Ekistoclecio <ekistoclecio1999@gmail.com>
2025-09-20 17:28:12 -0300

    feat: Adiciona a prop 'noBackground' ao componente Container para permitir fundo transparente

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -4,18 +4,24 @@ import { twMerge } from "tailwind-merge";
 type ContainerProps<E extends React.ElementType = "div"> = {
   as?: E;
   className?: string;
+  noBackground?: boolean;
 } & React.ComponentPropsWithoutRef<E>;
 
 export function Container<E extends React.ElementType = "div">({
   as,
   className,
   children,
+  noBackground,
   ...rest
 }: ContainerProps<E>) {
   const Component = as || "div";
   return (
     <Component
-      className={twMerge("p-4 bg-white rounded shadow-sm", className)}
+      className={twMerge(
+        "p-4 bg-white rounded shadow-sm",
+        noBackground && "bg-transparent",
+        className
+      )}
       {...rest}
     >
       {children}
